10b8244e77dcba3e793590bcb5a5cb44493e4664
phuhoang <phuhoangcb97@gmail.com>
2021-05-13 15:02:37 +0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+   
+})
+});
+
+/** */
+
